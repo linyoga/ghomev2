ccaac14b67f874e7ee4e0bae0f607d50e49bd05c
linyoga <linyoga@gmail.com>
2015-10-28 21:40:13 +0800

    finish footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    @photos = Instagram.user_recent_media("2237983589", {:count => 1})
+    @photos = Instagram.user_recent_media("2237983589", {:count => 10})
   end
 
 end

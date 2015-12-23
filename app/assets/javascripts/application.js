a753697b60c1ceed750a42deefb71773e1ceb978
linyoga <linyoga@gmail.com>
2015-12-24 06:46:39 +0800

    fix sign out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 
-
+//= require_tree .
+// Add following lines.
+//= require jquery 
+//= require jquery_ujs
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :skip => :registrations
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
 
 
 
-  
+
 resources :posts
 
 namespace :admin do

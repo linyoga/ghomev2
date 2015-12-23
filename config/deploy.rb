3940ebb8033645b00e496e7356c756104bfed3fa
linyoga <linyoga@gmail.com>
2015-12-24 02:39:18 +0800

    try restart again

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,9 +40,11 @@ set :linked_files, %w(config/database.yml config/secrets.yml)
 # set :keep_releases, 5
 
 namespace :deploy do
-    task :restart, :roles => :web do
-    run "touch #{ current_path }/tmp/restart.txt"
-  end
+
+  desc "Restart Passenger app"
+    task :restart do
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+    end
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do

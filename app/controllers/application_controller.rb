2cdfd587a818f678124bf83f98fe7a5239f75b4e
linyoga <linyoga@gmail.com>
2015-12-24 04:26:52 +0800

    change ig photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def find_photos
-    @photos = Instagram.user_recent_media("2237983589", {:count => 10})
+    @photos = Instagram.user_recent_media("2237983589", {:count => 5})
   end
   
 end
